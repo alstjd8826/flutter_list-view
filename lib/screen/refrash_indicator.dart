6d225f36e107cd18dfa0d613979de45ea3c3947a
imminsung <htk4064@flexenm.com>
2023-08-26 19:01:59 +0900

    push test2

--- lib/screen/refrash_indicator.dart
+++ lib/screen/refrash_indicator.dart
@@ -14,7 +14,7 @@ class RefreshIndicatorScreen extends StatelessWidget {
         onRefresh: () async {
           // 서버요청
           await Future.delayed(
-            Duration(seconds: 5),
+            Duration(seconds: 3),
           );
         },
         child: ListView(
